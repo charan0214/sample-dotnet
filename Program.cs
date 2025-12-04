4392068cc4a2fea25b59476f459801c2c63e7c75
charan0214 <charancherry112003@gmail.com>
2025-12-04 11:50:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello! Your .NET Web App is running.");
+
+app.Run();
